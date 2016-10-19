c5160fe67b970cf1c35061f6b21f085d8a1b4489
pizzd <davide.pizzocri@polimi.it>
2016-10-18 22:52:11 -0600

    Add files via upload
    Sketch of the "main.C", in order to start the development.

---
+++ MainSCIANTIX.C
@@ -0,0 +1,20 @@
+///////////////////////////////////////////
+//                                       //
+//           S C I A N T I X             //
+//           ---------------             //
+//                                       //
+//  Version: 0.1                         //
+//  Year   : 2016                        //
+//  Authors: D. Pizzocri and T. Barani   //
+//                                       //
+///////////////////////////////////////////
+
+/// SCIANTIX is a 0D code developed at Politecnico di Milano.
+/// The object of SCIANTIX is to represent the behaviour of a single grain of nuclear fuel.
+/// The modelling of inert gas behaviour is the main aspect considered.
+
+int main( )
+{
+
+  return 0;
+}
