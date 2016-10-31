e6cf41788626c0ce40938b3f4d8ab7c9c417222d
tbgalois <tommasobarani@polimi.it>
2016-10-31 07:29:45 +0100

    Setting things up for pizzd commit

--- src/MainSCIANTIX.C
+++ src/MainSCIANTIX.C
@@ -10,11 +10,29 @@
 ///////////////////////////////////////////
 
 /// SCIANTIX is a 0D code developed at Politecnico di Milano.
-/// The object of SCIANTIX is to represent the behaviour of a single grain of nuclear fuel.
+/// The objective of SCIANTIX is to represent the behaviour of a single grain of nuclear fuel.
 /// The modelling of inert gas behaviour is the main aspect considered.
+
+#include "GlobalVariables.h"
+#include "InputStorage.h"
+#include "InputInterpolator.h"
 
 int main( )
 {
 
+  while ( Time_h <= Time_end_h )
+  {
+	InputStorage( );
+    Temperature[1] = InputInterpolator( Time_h, Time_temperature_input, Temperature_input, Temperature_input_points );
+
+    if ( Time_h < Time_end_h )
+	{
+	  Time_step_number++;
+	  Time_h += dTime_h;
+  	  Time_s += dTime_s;
+	}
+	else break;
+  }
+	  
   return 0;
 }
