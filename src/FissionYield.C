8b9c6448201f6a3fe879d6de83e7457272dd2546
Davide Pizzocri <davide.pizzocri@gmail.com>
2017-03-28 15:53:05 +0000

    Upload new file

---
+++ src/FissionYield.C
@@ -0,0 +1,21 @@
+///////////////////////////////////////////
+//                                       //
+//           S C I A N T I X             //
+//           ---------------             //
+//                                       //
+//  Version: 0.1                         //
+//  Year   : 2016                        //
+//  Authors: D. Pizzocri and T. Barani   //
+//                                       //
+///////////////////////////////////////////
+
+/// FissionYield
+/// This routine collects the fission yields of several
+/// element/isotopes.
+/// If possible, it is recommended to add references
+/// as new yields are included.
+
+#include "FissionYield.h"
+
+const double Fission_yield_Xe(0.27);
+const double Fission_yield_Kr(0.03);
