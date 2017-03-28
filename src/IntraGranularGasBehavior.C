1306aebccb64ee7ce2549895c30e9b69b2ca3b6f
Davide Pizzocri <davide.pizzocri@gmail.com>
2017-03-28 15:58:11 +0000

    Upload new file

---
+++ src/IntraGranularGasBehavior.C
@@ -0,0 +1,19 @@
+///////////////////////////////////////////
+//                                       //
+//           S C I A N T I X             //
+//           ---------------             //
+//                                       //
+//  Version: 0.1                         //
+//  Year   : 2016                        //
+//  Authors: D. Pizzocri and T. Barani   //
+//                                       //
+///////////////////////////////////////////
+
+#include "IntraGranularGasBehavior.h"
+
+void IntraGranularGasBehavior( )
+{
+  GasProduction( );
+
+  GasDiffusion( );
+}
